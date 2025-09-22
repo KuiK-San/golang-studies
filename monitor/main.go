89b514ff6f3c2c838703a6b0cf56f336bcff0627
Guilherme Casagrande <thegui4000@gmail.com>
2025-09-22 15:31:29 -0300

    aula2

--- monitor/main.go
+++ monitor/main.go
@@ -1,13 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	exibeMenu()
+	option := captaOpcao()
+
+	switch option {
+	case 1:
+		fmt.Println("Monitoramento Iniciado")
+	case 2:
+		fmt.Println("Logs exibidos")
+	case 0:
+		os.Exit(0)
+	default:
+		fmt.Println(option, "Não é esperado")
+		os.Exit(-1)
+	}
+}
+
+func exibeMenu() {
 	fmt.Println("1 - Iniciar Monitoramento")
 	fmt.Println("2 - Exibir Logs")
 	fmt.Println("0 - Sair do programa")
+}
 
+func captaOpcao() int {
 	var option int
 	fmt.Scan(&option)
 	fmt.Println("Foi escolhida a opção", option, "no endereco de memoria", &option)
+
+	return option
 }
